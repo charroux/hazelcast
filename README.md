811746e1dd62aa39fa8905cd7b0bc859dec1baaf
benoit.charroux <benoit.charroux@EFREI89124M2-P.campus.villejuif>
2018-12-29 15:28:03 +0100

    docker compose

--- README.md
+++ README.md
@@ -1,14 +1,28 @@
 # hazelcast
 
-docker run hazelcast/hazelcast:3.11.1
+## Use Docker compose
 
-Members {size:1, ver:1} [
-        Member [<host_ip>]:5701 - 798ee578-f1a9-452b-b1d6-c32e8a2ef1b7 this
-]
+will start Hazelcast and a rest web service in front of Hazelcast:
 
-get the container ID adress: <host_ip> and use it: 
+gradlew build or ./gradlew build (on Linux)
 
-docker run --name hazelcastcaching -e JAVA_OPTS="-Dhazelcast.local.publicAddress=<host_ip>:5701" -p 5701:5701 hazelcast/hazelcast:3.11.1
+creates an executable jar file for the rest web service
+
+docker-compose up --build
+
+build is used to force the creation of a new image for the rest web service.
+
+Test the rest web service in a web browser: 
+
+http://localhost:8080/city/paris
+
+http://localhost:8080/city
+
+## Alternative to Docker Compose
+
+Start Hazelcast: 
+
+docker run --name hazelcastcaching -p 5701:5701 hazelcast/hazelcast:3.11.1
 
 Build the rest web service:
 
@@ -18,9 +32,17 @@ Create the Docker image (cqrs as the image name):
 
 docker build -t caching .
 
+with this Docker image: https://github.com/charroux/hazelcast/blob/master/Dockerfile
+
 docker run --link hazelcastcaching:hazelcast -p 8080:8181 caching
 
+Test the app with: 
+
 http://localhost:8080/city/paris
+
+http://localhost:8080/city
+
+## Tools for stopping and removing running containers
 
 docker container ps
 
--- src/main/java/com/example/caching/HazelcastConfig.java
+++ src/main/java/com/example/caching/HazelcastConfig.java
@@ -6,6 +6,7 @@ import com.hazelcast.config.GroupConfig;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.security.UsernamePasswordCredentials;
 import com.hazelcast.spring.cache.HazelcastCacheManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.cache.interceptor.SimpleKeyGenerator;
@@ -15,15 +16,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HazelcastConfig {
 
+    @Value( "${spring.hazelcast.host}" )
+    private String hazelcastHost;
+
+
     @Bean
     ClientConfig clientConfig(){
         ClientConfig config = new ClientConfig();
-        //config.getNetworkConfig().addAddress("127.0.0.1");
-        config.getNetworkConfig().addAddress("hazelcast");
-/*
-        config.setCredentials(new UsernamePasswordCredentials("dev", "dev-pass"));
-        GroupConfig groupConfig = config.getGroupConfig();
-        groupConfig.setName("dev");*/
+        config.getNetworkConfig().addAddress(hazelcastHost);
         return config;
     }
 
@@ -39,11 +39,6 @@ public class HazelcastConfig {
 
     @Bean
     public HazelcastInstance hazelcastInstance() {
-        System.out.println("------------------------------------");
-        System.out.println("------------------------------------");
-        System.out.println("------------------------------------");
-        System.out.println("------------------------------------");
-        System.out.println("------------------------------------");
         // for client HazelcastInstance LocalMapStatistics will not available
         return HazelcastClient.newHazelcastClient(clientConfig());
         // return Hazelcast.newHazelcastInstance();
